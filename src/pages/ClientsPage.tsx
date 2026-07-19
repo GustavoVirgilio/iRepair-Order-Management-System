3e424a7ee26434bf78f6a3eb6f2f4f2c1e528e5d
Gustavo Virgílio <gustavo.virg12@gmail.com>
2026-07-19 08:52:49 -0300

    feat: implementa ClientsPage com busca de clients via useEffect e loading state, ligada ao swagger

--- src/pages/ClientsPage.tsx
+++ src/pages/ClientsPage.tsx
@@ -1,5 +1,39 @@
-function ClientPage() {
-    return <h1>Clients</h1>
+import { useState, useEffect } from 'react';
+import type { Client } from '../types';
+import { getAllClients } from '../services/clientService';
+
+function ClientsPage() {
+    const [clients, setClients] = useState<Client[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        async function loadClients() {
+            const data = await getAllClients();
+            setClients(data);
+            setIsLoading(false);
+        }
+        loadClients();
+    }, []);
+
+    return (
+        <div>
+            <h1> Clients </h1>
+            {isLoading ? (
+                <p> Carregando... </p>
+            ) : (
+                <div> 
+                    {clients.map((client) => (
+                        <div key={client.id}>
+                            <p>{client.name}</p>
+                            <p>{client.phone}</p>
+                            <p>{client.email}</p>
+                            <p>{client.created_at}</p>
+                        </div>
+                    ))}
+        </div>
+    )}
+    </div>
+    );
 }
 
-export default ClientPage;
+export default ClientsPage;
